9c591d120f12b6085dbd4a10fcc9a19dff2dec72
toto <k023g0060@g.neec.ac.jp>
2025-02-06 13:11:04 +0900

    一旦

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
-﻿#include<stdio.h>
+#include<stdio.h>
 
+template <typename T0,typename T1>
+class Compare {
+public:
+	T0 Min(const T0& a, const T1& b) {
+		return a < b ? a : b;
+	};
+};
 int main() {
-
-	printf("Hello World");
-	printf("こんにちは");
+	
+	
 
 	return 0;
 }
