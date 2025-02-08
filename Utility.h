e673365d828db573c248fad2d0d7cf792ba26f65
toto <k023g0060@m.neec.ac.jp>
2025-02-09 01:07:07 +0900

    完了

--- Utility.h
+++ Utility.h
@@ -1,9 +1,11 @@
 #pragma once
-
 // スマートポインタ用
 #include <memory>
-
 #include <iostream>
+#include <numbers>
+
+const float PI = std::numbers::pi_v<float>;
+
 // 出力関数
 template <typename T>
 void CharOut(const std::string& text, const T& instance) {
--- main.cpp
+++ main.cpp
@@ -1,36 +1,49 @@
 #include "Utility.h"
 
 // 基底クラス
-class Animal {
+class IShape {
 public:
-	~Animal() = default;
-    virtual void Speak() const = 0;
+	virtual ~IShape() = default;
+	virtual void Draw() const = 0;
+	virtual float Size() const = 0;
+protected:
+	float size_ = 0.0f;
 };
 
-// 犬派生クラス
-class Dog : public Animal {
+// 派生クラス
+class Circle : public IShape {
 public:
-	void Speak() const override {
-		CharOut("Dog: Woof!");
+	void Draw() const override {
+		CharOut("Circle Draw",Size());
 	}
+	float Size() const override {
+		return PI * radius_ * radius_;
+	}
+private:
+	float radius_ = 3.0f;
 };
 
-// 猫派生クラス
-class Cat : public Animal {
+// 派生クラス
+class Rectangle : public IShape {
 public:
-	void Speak() const override {
-		CharOut("Cat: Meow!");
+	void Draw() const override {
+		CharOut("Rectangle Draw",Size());
 	}
+	float Size() const override {
+		return width_ * height_;
+	}
+private:
+	float width_ = 5.0f;
+	float height_ = 3.0f;
 };
 
 int main() {
-	// 生成
-    std::unique_ptr<Animal> dog = std::make_unique<Dog>();
-	std::unique_ptr<Animal> cat = std::make_unique<Cat>();
 
-	// 鳴く
-	dog->Speak();
-	cat->Speak();
+	std::unique_ptr<IShape> circle = std::make_unique<Circle>();
+	std::unique_ptr<IShape> rectangle = std::make_unique<Rectangle>();
 
-    return 0;
+	circle->Draw();
+	rectangle->Draw();
+
+	return 0;
 }
