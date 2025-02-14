9a86615089e80e23adadf6b9ad477783419e0da6
toto <k023g0060@g.neec.ac.jp>
2025-02-14 10:34:09 +0900

    test

--- main.cpp
+++ main.cpp
@@ -1,38 +1,96 @@
 #include <iostream>
 #include <thread>
+#include <vector>
+#include <queue>
+#include <functional>
 #include <mutex>
 #include <condition_variable>
-#include <stdio.h>
+#include <atomic>
 
+class ThreadManager {
+public:
+    ThreadManager(size_t numThreads);
+    ~ThreadManager();
+
+    void EnqueueTask(std::function<void()> task);
+    void StopAllThreads();
+
+private:
+    void WorkerThread(); // ワーカースレッドのメイン処理
+
+    std::vector<std::thread> workers_;
+    std::queue<std::function<void()>> tasks_;
+    std::mutex queueMutex_;
+    std::condition_variable condition_;
+    std::atomic<bool> stop_{ false };
+};
+
+// コンストラクタ: 指定した数のスレッドを作成
+ThreadManager::ThreadManager(size_t numThreads) {
+    for (size_t i = 0; i < numThreads; ++i) {
+        workers_.emplace_back([this] { WorkerThread(); });
+    }
+}
+
+// デストラクタ: スレッドを停止して解放
+ThreadManager::~ThreadManager() {
+    StopAllThreads();
+}
+
+// タスクをキューに追加
+void ThreadManager::EnqueueTask(std::function<void()> task) {
+    {
+        std::lock_guard<std::mutex> lock(queueMutex_);
+        tasks_.push(std::move(task));
+    }
+    condition_.notify_one(); // スレッドにタスクが追加されたことを通知
+}
+
+// ワーカースレッドの処理ループ
+void ThreadManager::WorkerThread() {
+    while (true) {
+        std::function<void()> task;
+        {
+            std::unique_lock<std::mutex> lock(queueMutex_);
+            condition_.wait(lock, [this] { return stop_ || !tasks_.empty(); });
+
+            if (stop_ && tasks_.empty()) return; // 停止フラグが立ったら終了
+
+            task = std::move(tasks_.front());
+            tasks_.pop();
+        }
+        task(); // タスクを実行
+    }
+}
+
+// 全スレッドの停止
+void ThreadManager::StopAllThreads() {
+    {
+        std::lock_guard<std::mutex> lock(queueMutex_);
+        stop_ = true;
+    }
+    condition_.notify_all(); // すべてのスレッドを起こす
+
+    for (std::thread& worker : workers_) {
+        if (worker.joinable()) {
+            worker.join();
+        }
+    }
+}
+
+// 使用例
 int main() {
-    std::mutex mtx;
-    std::condition_variable cv;
-    int currentThread = 1; // 実行中のスレッドを示す番号
+    ThreadManager threadManager(4); // 4スレッドを管理
 
-    // スレッドで実行するラムダ関数
-    auto printThread = [&mtx, &cv, &currentThread](int id) {
-        std::unique_lock<std::mutex> lock(mtx);
+    // タスクを追加
+    for (int i = 0; i < 10; ++i) {
+        threadManager.EnqueueTask([i] {
+            std::cout << "Task " << i << " executed on thread " << std::this_thread::get_id() << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            });
+    }
 
-        // 自分の順番になるまで待機
-        cv.wait(lock, [&] { return currentThread == id; });
-
-        // 順番が来たらメッセージを表示
-        printf("thread%d\n", id);
-
-        // 次のスレッドに進む
-        currentThread++;
-        cv.notify_all(); // 他のスレッドに通知
-        };
-
-    // スレッドを作成
-    std::thread t1(printThread, 1);
-    std::thread t2(printThread, 2);
-    std::thread t3(printThread, 3);
-
-    // スレッドの終了を待機
-    t1.join();
-    t2.join();
-    t3.join();
-
+    std::this_thread::sleep_for(std::chrono::seconds(2)); // 少し待つ
+    threadManager.StopAllThreads(); // 終了処理
     return 0;
 }
